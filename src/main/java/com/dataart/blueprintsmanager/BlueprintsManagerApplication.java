cc21a246a8d323745a3e69d0e8167a9c269937d6
spuzakov <sergey.puzakov@dataart.com>
2022-02-01 17:44:52 +0300

    Initial Commit

---
+++ src/main/java/com/dataart/blueprintsmanager/BlueprintsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dataart.blueprintsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueprintsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueprintsManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dataart/blueprintsmanager/BlueprintsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataart.blueprintsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueprintsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
